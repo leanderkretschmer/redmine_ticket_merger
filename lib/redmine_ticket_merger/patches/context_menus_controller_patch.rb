61702b6ac43bb8ef38e1ec478ac063fb5cd3d6ea
leanderkretschmer <leanderkretschmer@icloud.com>
2025-08-26 11:23:15 +0200

    version 2.6.1

--- lib/redmine_ticket_merger/patches/context_menus_controller_patch.rb
+++ lib/redmine_ticket_merger/patches/context_menus_controller_patch.rb
@@ -3,27 +3,15 @@ module RedmineTicketMerger
     module ContextMenusControllerPatch
       def self.included(base)
         base.class_eval do
-          # Kontextmenü für Issues erweitern
+          # Merge-Berechtigung zum bestehenden Kontextmenü hinzufügen
           def issues
-            @issues = Issue.visible.where(:id => params[:ids]).to_a
-            if (@issues.size == 1)
-              @issue = @issues.first
-            end
-            
-            @can = {:edit => @issues.collect{|c| c.editable?}.inject{|memo,word| memo && word},
-                   :log_time => (@project && User.current.allowed_to?(:log_time, @project)),
-                   :copy => User.current.allowed_to?(:add_issues, @project),
-                   :delete => @issues.collect{|c| c.deletable?}.inject{|memo,word| memo && word}
-                   }
+            # Bestehende Methode aufrufen
+            super
             
             # Merge-Berechtigung hinzufügen
-            @can[:merge] = @issues.size == 1 && 
-                          User.current.allowed_to?(:merge_issues, @project) && 
-                          !@issues.first.closed?
-            
-            @back = back_url
-            
-            render :layout => false
+            if @can && @issues && @issues.size == 1
+              @can[:merge] = User.current.allowed_to?(:merge_issues, @project) && !@issues.first.closed?
+            end
           end
         end
       end
