feaf3cefd4df26dadabdcf3c99f5db6af98a3385
leanderkretschmer <leanderkretschmer@icloud.com>
2025-08-26 10:24:30 +0200

    updated route register

---
+++ after_init.rb
@@ -0,0 +1,13 @@
+# Routes für das Ticket Merger Plugin registrieren
+# Diese Datei wird nach der Plugin-Initialisierung geladen
+
+Rails.application.routes.append do
+  resources :projects do
+    resources :issues, :only => [] do
+      collection do
+        get :merge_form
+        post :merge
+      end
+    end
+  end
+end
